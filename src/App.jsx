8d85a325dc3806cf172af037d30e1dfef4a5c2aa
mcqak <m.c.q.a.work@gmail.com>
2020-12-21 13:16:38 +0200

    navbar almost styled

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './assets/scss/main.scss'
+
+import MainNav from './components/MainNav'
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <MainNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainNav.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import '../assets/scss/navbar.scss'
+
+import { 
+    Nav,
+    NavDropdown,
+    Navbar,
+    Container
+} from 'react-bootstrap'
+
+const MainNav = () => {
+    return (
+        <Navbar variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Brand</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <NavDropdown title="Lists" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown className="active" title="Audiences" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Automate" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <span className="greetings">Hello</span>
+                        <NavDropdown className="profile-name" title="Michael" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+
+export default MainNav
